d5f9790e8a2651a5bceedbc4045bcb01dc9264a3
Marcelo Ramalho <marceloramalhocdev@gmail.com>
2026-04-19 19:43:49 -0300

    feat: wire the controllers that deal with cookies in the `Program.cs` and remove useless `Home` code

--- App/Program.cs
+++ App/Program.cs
@@ -1,10 +1,18 @@
 using App.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>
 (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/auth/login";
+        options.LogoutPath = "/auth/logout";
+    });
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -22,6 +30,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
