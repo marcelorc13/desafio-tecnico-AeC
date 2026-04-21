306007a4b92c6eb3f3e3a7991297f63ee804de47
Marcelo Ramalho <marceloramalhocdev@gmail.com>
2026-04-21 15:15:12 -0300

    fix: authenticated middleware redirect fix

--- App/Middlewares/RedirectAuthenticatedMiddleware.cs
+++ App/Middlewares/RedirectAuthenticatedMiddleware.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 namespace App.Middlewares;
 
 public class RedirectAuthenticatedMiddleware(RequestDelegate next)
@@ -9,6 +12,13 @@ public class RedirectAuthenticatedMiddleware(RequestDelegate next)
         if (context.User.Identity?.IsAuthenticated == true &&
             GuestOnlyPaths.Contains(context.Request.Path.Value?.ToLower()))
         {
+            var result = await context.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            if (!result.Succeeded || result.Properties?.ExpiresUtc <= DateTimeOffset.UtcNow)
+            {
+                await next(context);
+                return;
+            }
+
             context.Response.Redirect("/addresses");
             return;
         }
